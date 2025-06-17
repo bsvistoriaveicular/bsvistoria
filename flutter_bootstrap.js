04e6f230964cb9a5d8a6cd56b5a9692dea730d5c
WLPereira <washingtton_@hotmail.com>
2025-06-17 03:05:32 -0300

    build: ajuste na pagina

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "842572753"
+    serviceWorkerVersion: "1076300526"
   }
 });
