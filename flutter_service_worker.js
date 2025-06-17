533d21b5c148f4e9bc17a611b3ac7b2f0aadd4e3
WLPereira <washingtton_@hotmail.com>
2025-06-17 02:53:44 -0300

    build: ajuste na pagina

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -286,7 +286,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4e2b79f37f03c439132b00b19c035758
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "24686c9328169f398fed8cabb76833de",
+"flutter_bootstrap.js": "4b4c7ba684574f4045d1d717f15b7ccb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
